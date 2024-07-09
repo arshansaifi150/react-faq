d80c97919cfaf9a7abc1cb5fa2b3932f642a40e5
Mohd Arshan <110154295+arshansaifi150@users.noreply.github.com>
2024-07-09 14:02:25 +0000

    up

--- faq/src/App.jsx
+++ faq/src/App.jsx
@@ -5,27 +5,30 @@ function App() {
   
   const [drop,setDrop] = useState(false)
 
-  const handleSubmit = ()=>{
-    setDrop(prev=>!prev)
+  const handleSubmit = (index)=>{
+   return setDrop(drop===index?null:index)
   }
 
   return (
    <>
-   <div>
-    <div>
-      <div></div>
-      <div>
-        {faqData?.map((faq)=>(
-          <div onClick={handleSubmit}>
-            {faq.title}
+   <div className='h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+    <div className='w-[60%] min-h-[50%] py-10 shadow-3xl rounded-xl bg-slate-50 flex justify-around'>
+      <div className='text-3xl'><h1>Question and answer</h1></div>
+      <div className='flex flex-col break-normal gap-2'>
+       {faqData?.map((faq,index)=>(
+        <div className='shadow-xl border-2 rounded-md py-2 px-5 active:bg-red-300 '>
+          <div onClick={()=>(handleSubmit(index))} key={index} className='text-2xl delay-75'>
+            {faq.title} 
             
           </div>
-          {drop && (
-                <div>{faq.description}</div>
+            {drop===index &&
+            (
+              <div className=''>{faq.description}</div>
             )}
+          </div>
         ))}
       </div>
-    </div>
+          </div>
    </div>
    </>
   )
--- faq/src/data/faqData.js
+++ faq/src/data/faqData.js
@@ -1,32 +1,39 @@
 export const faqData =[
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
     {
+        
         title:"Do I have to allow use of Cookies?",
         description:"Yes, In order to use my page you have to allow cookies is your browser"
         
     },
+    
 ]
